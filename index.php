e0d8d421045eead130330b746edcf5e139ffb682
Saiful Rahman <sr.perante@gmail.com>
2024-05-30 16:43:15 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php require_once('config/database.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tebak Kata</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container my-4">
+        <h2 class="text-center mb-4">Permainan Tebak Kata</h2>
+        <div class="text-center">
+            <button class="btn btn-primary">Mulai Permainan</button>
+        </div>
+    </div>
+
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
